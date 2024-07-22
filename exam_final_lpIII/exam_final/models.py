3c70725ae8d70e0f4c7095bfa162312562e76d9d
Carlos P <2123110286@untels.edu.pe>
2024-07-22 15:21:10 -0500

    Peña Examen Final - Parte 2 - Base de Datos

--- exam_final_lpIII/exam_final/models.py
+++ exam_final_lpIII/exam_final/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Pena_Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=150)
+    SEXO_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+    ]
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
+    fecha_de_registro = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'{self.nombre} {self.apellidos}'
